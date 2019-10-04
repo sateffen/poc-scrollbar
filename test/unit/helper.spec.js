72142d6f7c195aeb3d1bdcaa9a521f4ac5c5cf25
sateffen <sateffen@gmail.com>
2019-10-04 20:20:21 +0200

    Extended tests for getWheelDeltaAsPixel

--- test/unit/helper.spec.js
+++ test/unit/helper.spec.js
@@ -1,3 +1,4 @@
+import {DOM_DELTA_LINE, DOM_DELTA_PAGE, DOM_DELTA_PIXEL, BROWSER_LINE_HEIGHT} from '../../src/constants';
 import {debounce, applyOptionsToScrollBarElement, getWheelDeltaAsPixel} from '../../src/helper';
 
 const originalWindowSetTimeout = window.setTimeout;
@@ -11,8 +12,73 @@ describe('Helper functions', () => {
     });
 
     describe('getWheelDeltaAsPixel', () => {
+        const scrollContainer = {clientWidth: 200, clientHeight: 100};
+
         it('should be a function', () => {
             expect(typeof getWheelDeltaAsPixel).toBe('function');
+        });
+
+        it('should return the deltaOption value if provided for X=true', () => {
+            const deltaOptionValue = Math.random();
+            const result = getWheelDeltaAsPixel(true, deltaOptionValue, DOM_DELTA_LINE, 4, scrollContainer);
+
+            expect(result).toBe(deltaOptionValue);
+        });
+
+        it('should return the deltaOption value if provided for X=false', () => {
+            const deltaOptionValue = Math.random();
+            const result = getWheelDeltaAsPixel(false, deltaOptionValue, DOM_DELTA_LINE, 4, scrollContainer);
+
+            expect(result).toBe(deltaOptionValue);
+        });
+
+        it('should return the deltaValue if the deltaMode is unknown', () => {
+            const deltaValue = Math.random();
+            const result = getWheelDeltaAsPixel(false, undefined, 'unknown', deltaValue, scrollContainer);
+
+            expect(result).toBe(deltaValue);
+        });
+
+        it('should return the expected result if the deltaMode is DOM_DELTA_PIXEL for X=true', () => {
+            const deltaValue = Math.random();
+            const result = getWheelDeltaAsPixel(true, undefined, DOM_DELTA_PIXEL, deltaValue, scrollContainer);
+
+            expect(result).toBe(deltaValue);
+        });
+
+        it('should return the expected result if the deltaMode is DOM_DELTA_PIXEL for X=false', () => {
+            const deltaValue = Math.random();
+            const result = getWheelDeltaAsPixel(false, undefined, DOM_DELTA_PIXEL, deltaValue, scrollContainer);
+
+            expect(result).toBe(deltaValue);
+        });
+
+        it('should return the expected result if the deltaMode is DOM_DELTA_PAGE for X=true', () => {
+            const deltaValue = 0.5;
+            const result = getWheelDeltaAsPixel(true, undefined, DOM_DELTA_PAGE, deltaValue, scrollContainer);
+
+            expect(result).toBe(deltaValue * scrollContainer.clientWidth);
+        });
+
+        it('should return the expected result if the deltaMode is DOM_DELTA_PAGE for X=false', () => {
+            const deltaValue = 0.5;
+            const result = getWheelDeltaAsPixel(false, undefined, DOM_DELTA_PAGE, deltaValue, scrollContainer);
+
+            expect(result).toBe(deltaValue * scrollContainer.clientHeight);
+        });
+
+        it('should return the expected result if the deltaMode is DOM_DELTA_LINE for X=true', () => {
+            const deltaValue = 5;
+            const result = getWheelDeltaAsPixel(true, undefined, DOM_DELTA_LINE, deltaValue, scrollContainer);
+
+            expect(result).toBe(deltaValue * BROWSER_LINE_HEIGHT);
+        });
+
+        it('should return the expected result if the deltaMode is DOM_DELTA_LINE for X=false', () => {
+            const deltaValue = 6;
+            const result = getWheelDeltaAsPixel(false, undefined, DOM_DELTA_LINE, deltaValue, scrollContainer);
+
+            expect(result).toBe(deltaValue * BROWSER_LINE_HEIGHT);
         });
     });
 
