f1581cea08fbb7d83798e092217022d9c6e3ae70
sateffen <sateffen@gmail.com>
2019-10-04 21:29:17 +0200

    Added comment in pocscrollbar test suite

--- test/unit/pocscrollbar.spec.js
+++ test/unit/pocscrollbar.spec.js
@@ -4,4 +4,6 @@ describe('PocScrollbar', () => {
     it('should be a class', () => {
         expect(typeof PocScrollbar).toBe('function');
     });
+
+    // Most stuff is tested by the integration tests, so here we don't test a lot
 });
